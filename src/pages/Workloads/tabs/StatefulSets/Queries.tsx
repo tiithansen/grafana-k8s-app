93608737aa0105381308a88a32120efa8b013768
Tiit Hansen <hansen.tiit@gmail.com>
2024-07-06 16:46:49 +0300

    refactor: Write queries using PromQL query builder

--- src/pages/Workloads/tabs/StatefulSets/Queries.tsx
+++ src/pages/Workloads/tabs/StatefulSets/Queries.tsx
@@ -2,34 +2,52 @@ import { SceneVariables } from "@grafana/scenes";
 import { Metrics } from "metrics/metrics";
 import { resolveVariable } from "common/variableHelpers";
 import { TableRow } from "./types";
+import { Labels, MatchOperators, PromQL } from "common/promql";
+
+export function createReplicasQuery(cluster: string, additionalLabels: Labels) {
+
+    return PromQL.max(
+        PromQL.metric(Metrics.kubeStatefulsetStatusReplicas.name)
+            .withLabels(additionalLabels)
+            .withLabelEquals('cluster', cluster)
+    ).by([
+        Metrics.kubeStatefulsetStatusReplicas.labels.statefulset
+    ])
+}
+
+export function createReplicasReadyQuery(cluster: string, additionalLabels: Labels) {
+    
+    return PromQL.max(
+        PromQL.metric(Metrics.kubeStatefulsetStatusReplicasReady.name)
+            .withLabels(additionalLabels)
+            .withLabelEquals('cluster', cluster)
+    ).by([
+        Metrics.kubeStatefulsetStatusReplicasReady.labels.statefulset
+    ])
+}
 
 export function createRowQueries(rows: TableRow[], sceneVariables: SceneVariables) {
 
-    const statefulSet = rows.map(row => row.statefulset).join('|');
+    const statefulSets = rows.map(row => row.statefulset).join('|');
     const cluster = resolveVariable(sceneVariables, 'cluster');
+
+    const additionalLabels = {
+        statefulset: {
+            operator: MatchOperators.MATCHES,
+            value: statefulSets
+        }
+    }
 
     return [
         {
             refId: 'replicas',
-            expr: `
-                max(
-                    ${Metrics.kubeStatefulsetStatusReplicas.name}{
-                        ${Metrics.kubeStatefulsetStatusReplicas.labels.statefulset}=~"${statefulSet}",
-                        cluster="${cluster}"
-                    }
-                ) by (${Metrics.kubeStatefulsetStatusReplicas.labels.statefulset})`,
+            expr: createReplicasQuery(cluster?.toString()!, additionalLabels).stringify(),
             instant: true,
             format: 'table'
         },
         {
             refId: 'replicas_ready',
-            expr: `
-                max(
-                    ${Metrics.kubeStatefulsetStatusReplicasReady.name}{
-                        ${Metrics.kubeStatefulsetStatusReplicasReady.labels.statefulset}=~"${statefulSet}",
-                        cluster="${cluster}"
-                    }
-                ) by (${Metrics.kubeStatefulsetStatusReplicasReady.labels.statefulset})`,
+            expr: createReplicasReadyQuery(cluster?.toString()!, additionalLabels).stringify(),
             instant: true,
             format: 'table'
         },
