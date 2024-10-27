71b2a1c0130621a438cedcf02cf7ba8396eec79a
Tiit Hansen <hansen.tiit@gmail.com>
2024-10-27 18:35:38 +0200

    refactor: Move node queries out from Nodes.tsx to Queries.tsx

--- src/pages/Clusters/tabs/Nodes/Nodes.tsx
+++ src/pages/Clusters/tabs/Nodes/Nodes.tsx
@@ -1,20 +1,17 @@
 import { 
     EmbeddedScene,
     SceneFlexLayout, 
-    SceneFlexItem, 
-    SceneQueryRunner,
+    SceneFlexItem,
     TextBoxVariable,
     VariableValueSelectors,
     SceneVariableSet,
-    SceneVariables,
 } from '@grafana/scenes';
-import { createRowQueries } from './Queries';
+import { NodesQueryBuilder } from './Queries';
 import { buildExpandedRowScene } from './NodeExpandedRowScene';
 import { getSeriesValue } from 'common/seriesHelpers';
-import { Metrics } from 'metrics/metrics';
 import { TextColor } from 'common/types';
 import { TableRow } from './types';
-import { AsyncTable, Column, ColumnSortingConfig, QueryBuilder } from 'components/AsyncTable';
+import { AsyncTable, Column } from 'components/AsyncTable';
 import { SortingState } from 'common/sortingHelpers';
 import { prefixRoute } from 'utils/utils.routing';
 import { ROUTES } from '../../../../constants';
@@ -226,38 +223,6 @@ function asyncDataRowMapper(row: TableRow, asyncRowData: Record<string, number[]
     }
 
     row.pod_count = getSeriesValue(asyncRowData, 'pod_count', serieMatcherByNodeNamePredicate(row))
-}
-
-class NodesQueryBuilder implements QueryBuilder<TableRow> {
-    rootQueryBuilder(variables: SceneVariableSet | SceneVariables, sorting: SortingState, sortingConfig?: ColumnSortingConfig<TableRow> | undefined) {
-        return new SceneQueryRunner({
-            datasource: {
-                uid: '$datasource',
-                type: 'prometheus',
-            },
-            queries: [
-                {
-                    refId: 'nodes',
-                    expr: `
-                        group(
-                            ${Metrics.kubeNodeInfo.name}{
-                                cluster="$cluster",
-                                ${Metrics.kubeNodeInfo.labels.node}=~".*$search.*"
-                            }
-                        ) by (
-                            ${Metrics.kubeNodeInfo.labels.internalIP},
-                            ${Metrics.kubeNodeInfo.labels.node},
-                            cluster
-                        )`,
-                    instant: true,
-                    format: 'table'
-                },
-            ], 
-        })
-    }
-    rowQueryBuilder(rows: TableRow[], variables: SceneVariableSet | SceneVariables) {
-        return createRowQueries(rows, variables)
-    }
 }
 
 export const getNodesScene = () => {
--- src/pages/Clusters/tabs/Nodes/Queries.tsx
+++ src/pages/Clusters/tabs/Nodes/Queries.tsx
@@ -1,124 +1,155 @@
-import { SceneVariables } from "@grafana/scenes";
+import { SceneQueryRunner, SceneVariableSet, SceneVariables } from "@grafana/scenes";
 import { resolveVariable } from "common/variableHelpers";
 import { Metrics } from "metrics/metrics";
 import { TableRow } from "./types";
+import { ColumnSortingConfig, QueryBuilder } from "components/AsyncTable";
+import { SortingState } from "common/sortingHelpers";
 
-export function createRowQueries(rows: TableRow[], sceneVariables: SceneVariables) {
+export class NodesQueryBuilder implements QueryBuilder<TableRow> {
 
-    const nodes = rows.map(row => row.internal_ip + ":.*").join('|');
-    const nodeNames = rows.map(row => row.node).join('|');
-    const cluster = resolveVariable(sceneVariables, 'cluster');
+    rootQueryBuilder(variables: SceneVariableSet | SceneVariables, sorting: SortingState, sortingConfig?: ColumnSortingConfig<TableRow> | undefined) {
+        return new SceneQueryRunner({
+            datasource: {
+                uid: '$datasource',
+                type: 'prometheus',
+            },
+            queries: [
+                {
+                    refId: 'nodes',
+                    expr: `
+                        group(
+                            ${Metrics.kubeNodeInfo.name}{
+                                cluster="$cluster",
+                                ${Metrics.kubeNodeInfo.labels.node}=~".*$search.*"
+                            }
+                        ) by (
+                            ${Metrics.kubeNodeInfo.labels.internalIP},
+                            ${Metrics.kubeNodeInfo.labels.node},
+                            cluster
+                        )`,
+                    instant: true,
+                    format: 'table'
+                },
+            ], 
+        })
+    }
 
-    return [
-       {
-            refId: 'memory_total',
-            expr: `
-                max(
-                    ${Metrics.nodeMemoryMemTotalBytes.name}{
-                        ${Metrics.nodeMemoryMemTotalBytes.labels.instance}=~"${nodes}",
-                        cluster="${cluster}"
-                    }
-                ) by (
-                    ${Metrics.nodeMemoryMemTotalBytes.labels.instance},
-                    cluster
-                )`,
-            instant: true,
-            format: 'table'
-       },
-       {
-            refId: 'memory_free',
-            expr: `
-                max(
-                    ${Metrics.nodeMemoryMemAvailableBytes.name}{
-                        ${Metrics.nodeMemoryMemAvailableBytes.labels.instance}=~"${nodes}",
-                        cluster="${cluster}"
-                    }
-                ) by (
-                    ${Metrics.nodeMemoryMemAvailableBytes.labels.instance},
-                    cluster
-                )`,
-            instant: true,
-            format: 'table'
-        },
-        {
-            refId: 'memory_requests',
-            expr: `
-                sum(
-                    ${Metrics.kubePodContainerResourceRequests.name}{
-                        ${Metrics.kubePodContainerResourceRequests.labels.resource}="memory",
-                        ${Metrics.kubePodContainerResourceRequests.labels.node}=~"${nodeNames}",
-                        ${Metrics.kubePodContainerResourceRequests.labels.container}!="",
-                        cluster="${cluster}"
-                    }
-                ) by (${Metrics.kubePodContainerResourceRequests.labels.node})`,
-            instant: true,
-            format: 'table'
-        },
-        {
-            refId: 'cores',
-            expr: `
-                max(
-                    ${Metrics.machineCpuCores.name}{
-                        ${Metrics.machineCpuCores.labels.node}=~"${nodeNames}",
-                        cluster="${cluster}"
-                    }
-                ) by (${Metrics.machineCpuCores.labels.node})`,    
-            instant: true,
-            format: 'table'
-       },
-       {
-            refId: 'cpu_requests',
-            expr: `
-                sum(
-                    ${Metrics.kubePodContainerResourceRequests.name}{
-                        ${Metrics.kubePodContainerResourceRequests.labels.resource}="cpu",
-                        ${Metrics.kubePodContainerResourceRequests.labels.node}=~"${nodeNames}",
-                        ${Metrics.kubePodContainerResourceRequests.labels.container}!="",
-                        cluster="${cluster}"
-                    }
-                ) by (${Metrics.kubePodContainerResourceRequests.labels.node})`,
-            instant: true,
-            format: 'table'
-        },
-        {
-            refId: 'cpu_usage',
-            expr: `
-                (
-                    sum by(${Metrics.nodeCpuSecondsTotal.labels.instance}) (
-                        irate(
-                            ${Metrics.nodeCpuSecondsTotal.name}{
-                                ${Metrics.nodeCpuSecondsTotal.labels.instance}=~"${nodes}",
-                                ${Metrics.nodeCpuSecondsTotal.labels.mode}!="idle",
+    rowQueryBuilder(rows: TableRow[], variables: SceneVariableSet | SceneVariables) {
+        const nodes = rows.map(row => row.internal_ip + ":.*").join('|');
+        const nodeNames = rows.map(row => row.node).join('|');
+        const cluster = resolveVariable(variables, 'cluster');
+
+        return [
+            {
+                    refId: 'memory_total',
+                    expr: `
+                        max(
+                            ${Metrics.nodeMemoryMemTotalBytes.name}{
+                                ${Metrics.nodeMemoryMemTotalBytes.labels.instance}=~"${nodes}",
                                 cluster="${cluster}"
-                            }[$__rate_interval]
-                        )
-                    )
-                    /
-                    on (${Metrics.nodeCpuSecondsTotal.labels.instance}) group_left sum by (${Metrics.nodeCpuSecondsTotal.labels.instance}) (
-                        (
+                            }
+                        ) by (
+                            ${Metrics.nodeMemoryMemTotalBytes.labels.instance},
+                            cluster
+                        )`,
+                    instant: true,
+                    format: 'table'
+            },
+            {
+                    refId: 'memory_free',
+                    expr: `
+                        max(
+                            ${Metrics.nodeMemoryMemAvailableBytes.name}{
+                                ${Metrics.nodeMemoryMemAvailableBytes.labels.instance}=~"${nodes}",
+                                cluster="${cluster}"
+                            }
+                        ) by (
+                            ${Metrics.nodeMemoryMemAvailableBytes.labels.instance},
+                            cluster
+                        )`,
+                    instant: true,
+                    format: 'table'
+                },
+                {
+                    refId: 'memory_requests',
+                    expr: `
+                        sum(
+                            ${Metrics.kubePodContainerResourceRequests.name}{
+                                ${Metrics.kubePodContainerResourceRequests.labels.resource}="memory",
+                                ${Metrics.kubePodContainerResourceRequests.labels.node}=~"${nodeNames}",
+                                ${Metrics.kubePodContainerResourceRequests.labels.container}!="",
+                                cluster="${cluster}"
+                            }
+                        ) by (${Metrics.kubePodContainerResourceRequests.labels.node})`,
+                    instant: true,
+                    format: 'table'
+                },
+                {
+                    refId: 'cores',
+                    expr: `
+                        max(
+                            ${Metrics.machineCpuCores.name}{
+                                ${Metrics.machineCpuCores.labels.node}=~"${nodeNames}",
+                                cluster="${cluster}"
+                            }
+                        ) by (${Metrics.machineCpuCores.labels.node})`,    
+                    instant: true,
+                    format: 'table'
+            },
+            {
+                refId: 'cpu_requests',
+                expr: `
+                    sum(
+                        ${Metrics.kubePodContainerResourceRequests.name}{
+                            ${Metrics.kubePodContainerResourceRequests.labels.resource}="cpu",
+                            ${Metrics.kubePodContainerResourceRequests.labels.node}=~"${nodeNames}",
+                            ${Metrics.kubePodContainerResourceRequests.labels.container}!="",
+                            cluster="${cluster}"
+                        }
+                    ) by (${Metrics.kubePodContainerResourceRequests.labels.node})`,
+                instant: true,
+                format: 'table'
+            },
+            {
+                refId: 'cpu_usage',
+                expr: `
+                    (
+                        sum by(${Metrics.nodeCpuSecondsTotal.labels.instance}) (
                             irate(
                                 ${Metrics.nodeCpuSecondsTotal.name}{
                                     ${Metrics.nodeCpuSecondsTotal.labels.instance}=~"${nodes}",
-                                    cluster="${cluster}",
+                                    ${Metrics.nodeCpuSecondsTotal.labels.mode}!="idle",
+                                    cluster="${cluster}"
                                 }[$__rate_interval]
                             )
                         )
-                    )
-                ) * 100`,
-            instant: true,
-            format: 'table'
-        },
-        {
-            refId: 'pod_count',
-            expr: `
-                count(
-                    ${Metrics.kubePodInfo.name}{
-                        ${Metrics.kubePodInfo.labels.node}=~"${nodeNames}",
-                        cluster="${cluster}"
-                    }
-                ) by (${Metrics.kubePodInfo.labels.node})`,
-            instant: true,
-            format: 'table'
-        }
-    ];
+                        /
+                        on (${Metrics.nodeCpuSecondsTotal.labels.instance}) group_left sum by (${Metrics.nodeCpuSecondsTotal.labels.instance}) (
+                            (
+                                irate(
+                                    ${Metrics.nodeCpuSecondsTotal.name}{
+                                        ${Metrics.nodeCpuSecondsTotal.labels.instance}=~"${nodes}",
+                                        cluster="${cluster}",
+                                    }[$__rate_interval]
+                                )
+                            )
+                        )
+                    ) * 100`,
+                instant: true,
+                format: 'table'
+            },
+            {
+                refId: 'pod_count',
+                expr: `
+                    count(
+                        ${Metrics.kubePodInfo.name}{
+                            ${Metrics.kubePodInfo.labels.node}=~"${nodeNames}",
+                            cluster="${cluster}"
+                        }
+                    ) by (${Metrics.kubePodInfo.labels.node})`,
+                instant: true,
+                format: 'table'
+            }
+        ];
+    }
 }
